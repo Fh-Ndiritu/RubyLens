d1d5cb14ac78c900ad636137ccd2018313d13c60
Fh-Ndiritu <fhndiritu@gmail.com>
2023-03-18 09:36:05 +0300

    Remove jobs CRUD routes not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :jobs
+  resources :jobs, only: [:index, :show]
   root to: 'jobs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
